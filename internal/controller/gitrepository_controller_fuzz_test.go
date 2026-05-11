03fdc600b366734592e41ae81bb3f2e4604041a1
estebany-qd <esteban.yusunguaira@qdrant.com>
2026-05-10 20:08:20 -0500

    fix: restore fuzz test and update storage API

--- internal/controller/gitrepository_controller_fuzz_test.go
+++ internal/controller/gitrepository_controller_fuzz_test.go
@@ -8,7 +8,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package controller
 
 import (
@@ -479,13 +480,7 @@ func startEnvServer(setupReconcilers func(manager.Manager)) *envtest.Environment
 		panic(err)
 	}
 	defer os.RemoveAll(tmpStoragePath)
-	artStorage, err = artstorage.New(&artcfg.Options{
-		StoragePath:              tmpStoragePath,
-		StorageAdvAddress:        "localhost:5050",
-		ArtifactRetentionTTL:     time.Minute * 1,
-		ArtifactRetentionRecords: 2,
-		ArtifactDigestAlgo:       artdigest.Canonical.String(),
-	})
+	artStorage, err = artstorage.New(&artcfg.Options{StoragePath: tmpStoragePath, StorageAdvAddress: "localhost:5050", ArtifactRetentionTTL: time.Minute, ArtifactRetentionRecords: 2, ArtifactDigestAlgo: artdigest.Canonical.String()})
 	if err != nil {
 		panic(err)
 	}
